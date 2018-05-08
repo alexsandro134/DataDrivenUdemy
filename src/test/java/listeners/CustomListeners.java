cabf1608f13ba6a8fea5320391d2d839591f76a6
Tai Le <alexsandro134@gmail.com>
2018-05-08 22:11:39 +0700

    add report when fail

--- src/test/java/listeners/CustomListeners.java
+++ src/test/java/listeners/CustomListeners.java
@@ -3,6 +3,7 @@ package listeners;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.Reporter;
 
 public class CustomListeners implements ITestListener{
 
@@ -22,7 +23,11 @@ public class CustomListeners implements ITestListener{
 	}
 
 	public void onTestFailure(ITestResult arg0) {
-		// TODO Auto-generated method stub
+		System.setProperty("org.uncommons.reportng.escape-output","false");
+		Reporter.log("Capturing screenshot");
+		Reporter.log("<a target=\"_blank\" href=\"E:\\Wallpapers\\wallhaven-16687.jpg\">Screenshot</a>");
+		Reporter.log("<br>");
+		Reporter.log("<a target=\"_blank\" href=\"E:\\Wallpapers\\wallhaven-16687.jpg\"><img src=\"E:\\Wallpapers\\wallhaven-16687.jpg\" height=200 width=200></img></a>");
 		
 	}
 
--- src/test/java/testcases/BankManagerLoginTest.java
+++ src/test/java/testcases/BankManagerLoginTest.java
@@ -11,16 +11,13 @@ public class BankManagerLoginTest extends TestBase{
 
 	@Test
 	public void loginAsBankManager() throws InterruptedException{
-		System.setProperty("org.uncommons.reportng.escape-output","false");
+		
 		log.debug("=========== Start loginTest ===========");
 		driver.findElement(By.xpath(OR.getProperty("bmlBtn"))).click();
 		
 		Assert.assertTrue(isElementPresent(By.xpath(OR.getProperty("addCustBtn"))));
 		
 		log.debug("=========== Successful loginTest ===========");
-		Reporter.log("LoginTest successful");
-		Reporter.log("<a target=\"_blank\" href=\"E:\\Wallpapers\\wallhaven-16687.jpg\">Screenshot</a>");
-		Reporter.log("<br>");
-		Reporter.log("<a target=\"_blank\" href=\"E:\\Wallpapers\\wallhaven-16687.jpg\"><img src=\"E:\\Wallpapers\\wallhaven-16687.jpg\" height=200 width=200></img></a>");
+		Assert.fail("Login not successful");
 	}
 }
